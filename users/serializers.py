bffe57ec825564864d6d77d7d0dc7e09feb3c1a8
mishustik-basket333 <iiiskhakov1990@gmail.com>
2023-09-21 23:15:32 +0300

    add serializer for registration in users

--- users/serializers.py
+++ users/serializers.py
@@ -6,8 +6,8 @@ from users.models import User
 from users.validators import TelegramIdValidator, EmailValidator
 
 
-class UserSerializer(serializers.ModelSerializer):
-    """ Сериализатор пользователя с полным отображением данных"""
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    """ Сериализатор пользователя для регистрации"""
 
     modules = ModulesSmallSerializer(source='id_users', read_only=True, many=True)
 
@@ -22,6 +22,21 @@ class UserSerializer(serializers.ModelSerializer):
 
     def validate_password(self, value: str) -> str:
         return make_password(value)
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """ Сериализатор пользователя с полным отображением данных"""
+
+    modules = ModulesSmallSerializer(source='id_users', read_only=True, many=True)
+
+    class Meta:
+        model = User
+        fields = ("id", "email", "chat_telegram_id", "first_name", "first_name", "last_name", 'phone', 'country',
+                  'city', "roles", "modules")
+        validators = [
+            TelegramIdValidator(field='chat_telegram_id'),
+            EmailValidator(field="email")
+        ]
 
 
 class UserPublishedSerializer(serializers.ModelSerializer):
